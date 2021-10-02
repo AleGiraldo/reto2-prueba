d1b706db620605a9c6c304e5134875ee34c99b64
Alejandro Giraldo <johncarlosarrieta@gmail.com>
2021-10-01 19:08:27 -0500

    Funciones js

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,26 @@
-function metodoGet(){
+function traerInformacion() {
     $.ajax({
-        url:"https://g468ced4b1f31f1-dbhotel.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/room/room",
-        headers:{'accept':'application/json', 'Access-Control-Allow-Origin' : '*'},
+        url:"https://g468ced4b1f31f1-dbhotel.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/room/room/",
         type:"GET",
         datatype:"JSON",
-        success:function(respuesta){
+        succes:function(respuesta){
             console.log(respuesta);
+            mostrarRespuesta(respuesta.items);
+            }
+        });
+}
 
-        }
-    });
+function mostrarRespuesta(items){
+
+    let myTable="<Table>";
+    for(i=0;i<items.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+items[i].id+"</td>";
+        myTable+="<td>"+items[i].room+"</td>";
+        myTable+="<td>"+items[i].stars+"</td>";
+        myTable+="<td>"+items[i].category_id+"</td>";
+        myTable+="<td>"+items[i].description+"</td>";
+    }
+    myTable+="</Table>"
+    $("#resultado").append(myTable);
 }
